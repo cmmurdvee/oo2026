a0c55a0c357a4603ca65d746467fcd1bae0617ea
Chris-Martin Murdvee <murdvee@tlu.ee>
2026-03-06 10:13:54 +0200

    kaustate loomine
    valmistumine kontrolltööks

---
+++ 05_06.03-kontrolltoo1/kontrolltoo/src/test/java/ee/murdvee/kontrolltoo/KontrolltooApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.murdvee.kontrolltoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontrolltooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
