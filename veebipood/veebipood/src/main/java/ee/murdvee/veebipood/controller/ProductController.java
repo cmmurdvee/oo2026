753db807bd0ff5828ff8760f769957954f62f214
Chris-Martin Murdvee <murdvee@tlu.ee>
2026-03-13 13:57:33 +0200

    CrossOrigin

--- veebipood/veebipood/src/main/java/ee/murdvee/veebipood/controller/ProductController.java
+++ veebipood/veebipood/src/main/java/ee/murdvee/veebipood/controller/ProductController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*") // turvaviga, p2ris arendustes seda ei teeks
+//@CrossOrigin(origins = "http://localhost:5173").
+//@CrossOrigin(origins = "https://www.arvutitark.ee").
 @RestController
 public class ProductController {
 
